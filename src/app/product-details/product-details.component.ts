40365caf937371224804ed5fa10fe8a16a6d1f7c
aidenharrington <aidenkharrington@gmail.com>
2019-09-04 10:12:04 -0700

    Removed bugs, product details still not working

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import {Component, OnInit } from '@angular/core';
+
 
 import { products } from '../products';
 import { CartService } from '../cart.service';
